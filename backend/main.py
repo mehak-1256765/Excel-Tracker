e5d0ab543d0211d3b4c926bc1f0ae2466fcf484c
mehak-1256765 <mehakmanhas19@gmail.com>
2026-05-04 21:38:02 +0530

    Updation

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "https://excel-tracker-five.vercel.app"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
